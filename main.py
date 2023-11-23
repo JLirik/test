135d1746574091e823b1c099938a59a7e623efe6
JLirik <dimdimych181007@yandex.ru>
2023-11-23 17:43:06 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QDialog, QApplication
+
+
+class Example(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.do_paint = False
+
+    def initUI(self):
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw(self, qp):
+        # Задаем кисть
+        qp.setBrush(QColor(255, 255, 0))
+        for i in range(5):
+            x = random.randint(0, 320)
+            y = random.randint(0, 460)
+            r = random.randint(5, 75)
+            qp.drawEllipse(x, y, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
